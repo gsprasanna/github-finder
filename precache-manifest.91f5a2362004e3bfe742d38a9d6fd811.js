3e81575d87ceba86ac3a6ff221af5b4cc2836060
Prasanna Balaji <erprasanna97@gmail.com>
2020-06-15 11:01:12 +0530

    Updates

---
+++ precache-manifest.91f5a2362004e3bfe742d38a9d6fd811.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7ee8a26d563a7dc955ebdc6adc2998",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "df51706c61376971cb61",
+    "url": "/github-finder/static/css/2.3761eb0c.chunk.css"
+  },
+  {
+    "revision": "2a9db18b64a2ea26b3e4",
+    "url": "/github-finder/static/css/main.600d1542.chunk.css"
+  },
+  {
+    "revision": "df51706c61376971cb61",
+    "url": "/github-finder/static/js/2.92133c86.chunk.js"
+  },
+  {
+    "revision": "a986d777e7ab357b3f79ca1eede38fb5",
+    "url": "/github-finder/static/js/2.92133c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a9db18b64a2ea26b3e4",
+    "url": "/github-finder/static/js/main.e0a14773.chunk.js"
+  },
+  {
+    "revision": "e25b70013f69521195e1",
+    "url": "/github-finder/static/js/runtime-main.f012ac68.js"
+  },
+  {
+    "revision": "81c29bc5316124abc2c07a92ebbc23e6",
+    "url": "/github-finder/static/media/loading.81c29bc5.gif"
+  }
+]);
